0131749cc356c02d08bb0013a2305fb16d16864c
TheNanO18 <ghosthuman18@gmail.com>
2025-05-05 20:22:41 +0900

    Json DB Connect

--- Back/myproject/myapp/serializers.py
+++ Back/myproject/myapp/serializers.py
@@ -1,13 +1,13 @@
+# serializers.py
 from rest_framework import serializers
-# from .model import Item
+from .models import Item, Info
 
-class ItemSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    name = serializers.CharField(max_length=100)
-    description = serializers.CharField(max_length=255)
-
-
-# class ItemSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Item
-#         fields = ['id', 'name', 'description']
+class ItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Item
+        fields = '__all__'  # 'id', 'name', 'description'이 자동으로 포함됨
+        
+class InfoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Info
+        fields = '__all__'
