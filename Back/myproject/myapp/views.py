b35149177bfa552e851dd88e7ab567b7f323e0f3
TheNanO18 <ghosthuman18@gmail.com>
2025-05-09 23:57:33 +0900

    Login Test

--- Back/myproject/myapp/views.py
+++ Back/myproject/myapp/views.py
@@ -1,3 +1,5 @@
+# views.py
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -6,37 +8,47 @@ from .serializers import ItemSerializer, InfoSerializer
 
 class BaseModelHandler(APIView):
     def handle_data(self, data_type, data):
-        """
-        데이터 유형에 맞는 모델과 serializer를 선택하여 처리하는 함수
-        """
         if data_type == 'item':
             serializer = ItemSerializer(data=data)
-            model = Item
+            if serializer.is_valid():
+                instance = serializer.save()
+                return serializer, instance
+            return None, serializer.errors
+
         elif data_type == 'info':
             serializer = InfoSerializer(data=data)
-            model = Info
-        else:
-            return None, None
+            if serializer.is_valid():
+                instance = serializer.save()
+                return serializer, instance
+            return None, serializer.errors
 
-        if serializer.is_valid():
-            instance = serializer.save()  # 데이터베이스에 저장
-            return serializer, instance
+        elif data_type == 'check_login':  # 👈 여기에 검증 추가
+            user_id = data.get('user_id')
+            password = data.get('password')
+            if user_id == 'admin' and password == '1234':
+                return {'message': 'Login successful'}, None
+            else:
+                return None, {'message': 'Invalid credentials'}
 
-        return None, serializer.errors
-    
+        return None, {'error': 'Unknown data_type'}
+
 class ItemUserProfileHandler(BaseModelHandler):
     def post(self, request):
         print("받은 데이터:", request.data)
 
         data_type = request.data.get('type')
-        data_content = request.data.get('data')  # 여기 추가!
+        data_content = request.data.get('data')
 
-        serializer, instance = self.handle_data(data_type, data_content)  # 수정된 부분!
+        serializer, instance = self.handle_data(data_type, data_content)
 
         if serializer:
+            # serializer일 수도 있고 dict일 수도 있음
+            response_data = (
+                serializer.data if hasattr(serializer, 'data') else serializer
+            )
             return Response({
-                'message': f'{data_type} 저장 완료!',
-                'data': serializer.data
-            }, status=status.HTTP_201_CREATED)
+                'message': f'{data_type} 처리 완료!',
+                'data': response_data
+            }, status=status.HTTP_200_OK)
 
         return Response(instance, status=status.HTTP_400_BAD_REQUEST)
