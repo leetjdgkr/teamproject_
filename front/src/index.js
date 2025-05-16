e30e754094b2f5f07896f1afb3692e88f4c65fa2
Namjinwoo-829 <kywlsdn@naver.com>
2025-05-16 23:27:19 +0900

    Front url test

--- front/src/index.js
+++ front/src/index.js
@@ -1,27 +1,35 @@
-import React,{ useState } from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Login from './login/login';
-import App from './App';
 import Calendar from './calenderTest/calenderFront/calender';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UserContext from "./login/js/userContext";
+import RequireAuth from "./requireauth"; // 분리한 인증 컴포넌트 import
 
-const Root = () =>{
+const Root = () => {
   const [user, setUser] = useState(null);
 
-  return(
-    <UserContext.Provider value={{user,setUser}}>
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
       <BrowserRouter basename="/">
         <Routes>
           <Route path="/" element={<Login />} />
-          <Route path="/data" element={<Calendar />} />
+          <Route
+            path="/data"
+            element={
+              <RequireAuth>
+                <Calendar />
+              </RequireAuth>
+            }
+          />
         </Routes>
       </BrowserRouter>
     </UserContext.Provider>
   );
-}
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Root />);
 
