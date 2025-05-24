1d151bf144381b5ff2b2fe57659a93b954f73e8f
rlawotjs158 <rlawotjs158@naver.com>
2025-05-24 19:22:12 +0900

    Front amin page

--- front/src/adminpage/adminPage.js
+++ front/src/adminpage/adminPage.js
@@ -1,23 +1,37 @@
 import React from "react";
+import "../adminpage/adminPage.css";
 
 const AdminPage = () => {
-    return(
-        <div className="adminpageBk">
-            <from>
-                <div className="mosqueName">
-                    <span>이름</span>
-                    <input></input>
-                </div>
-                <div className="mosqueBrithday">
-                    <span>이름</span>
-                    <input></input>
-                </div>
-                <div className="mosquePN">
-                    <span>전화번호</span>
-                    <input></input>
-                </div>
-            </from>                                   
+    const people = {
+        header:["이름","생년월일","전화번호"],
+        data: [
+            { people: "김재선", birthday: "2000.10.02", phoneNumber: "010-4134-9069" },
+            { people: "남진우", birthday: "2000.08.29", phoneNumber: "010-3954-7589" },
+        ]
+    };
+    
+    return (
+        <div>
+            <table>
+                <thead>
+                    {people.header.map ((item) =>{
+                        return(
+                            <th>{item}</th>
+                        )
+                    })}
+                </thead>
+                <tbody>
+                    {people.data.map((item, index) => (
+                        <tr key={index}>
+                            <td>{item.people}</td>
+                            <td>{item.birthday}</td>
+                            <td>{item.phoneNumber}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
-    )
-}
+    );
+};
+
 export default AdminPage;
